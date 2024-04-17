e9af536597356d207b56b1f7dc2e364c54f91097
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-04-17 17:49:03 +0200

    Add solution for problem 2769: Find the Maximum Achievable Number

---
+++ 2769. Find the Maximum Achievable Number.cs
@@ -0,0 +1,9 @@
+// Problem-Link : https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+// Problem-ID : 2769
+// Problem Name: Find the Maximum Achievable Number
+// Verdict: AC
+public class Solution {
+    public int TheMaximumAchievableX(int num, int t) {
+       return num+(2*t);
+    }
+}
